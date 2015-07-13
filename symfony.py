2c1a5943ec3a5cb0d3d3bc7f1e085b87b8cb71fb
giliam <matthieu.denoux@gmail.com>
2015-07-13 17:05:42 +0200

    Groups the methods and attributes by class.

--- symfony.py
+++ symfony.py
@@ -7,21 +7,25 @@ class SymfonyCommand(sublime_plugin.TextCommand):
 		attributes_mask = re.compile(r'(public|private|protected) \$(\w+);')
 		class_mask = re.compile(r'^class (\w+)')
 		functions = []
-		classes = []
 		attributes = []
-		display = ""
+		classes = ["_"]
+		functions.append([])
+		attributes.append([])
+		current_class = "_"
 
 		for line in open(self.view.file_name(),'r'):
 			line = line.strip()
 			results_functions = functions_mask.match(line)
 			results_class = class_mask.match(line)
 			results_attributes = attributes_mask.match(line)
-			if results_functions:
-				functions.append(results_functions.group(2))
 			if results_class:
 				classes.append(results_class.group(1))
+				functions.append([])
+				attributes.append([])
+			if results_functions:
+				functions[len(classes)-1].append(results_functions.group(2))
 			if results_attributes:
-				attributes.append(results_attributes.group(2))
+				attributes[len(classes)-1].append(results_attributes.group(2))
 		print(functions)
 		print(classes)
 		print(attributes)
