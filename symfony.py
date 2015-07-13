186ce8ad0adc4d9f0f274496dc49c48be1836cbc
giliam <matthieu.denoux@gmail.com>
2015-07-13 17:07:25 +0200

    Renames the functions variable.

--- symfony.py
+++ symfony.py
@@ -3,29 +3,29 @@ import sublime, sublime_plugin
 
 class SymfonyCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		functions_mask = re.compile(r'(public|private|protected) function (\w+)')
+		methods_mask = re.compile(r'(public|private|protected) function (\w+)')
 		attributes_mask = re.compile(r'(public|private|protected) \$(\w+);')
 		class_mask = re.compile(r'^class (\w+)')
-		functions = []
+		methods = []
 		attributes = []
 		classes = ["_"]
-		functions.append([])
+		methods.append([])
 		attributes.append([])
 		current_class = "_"
 
 		for line in open(self.view.file_name(),'r'):
 			line = line.strip()
-			results_functions = functions_mask.match(line)
+			results_methods = methods_mask.match(line)
 			results_class = class_mask.match(line)
 			results_attributes = attributes_mask.match(line)
 			if results_class:
 				classes.append(results_class.group(1))
-				functions.append([])
+				methods.append([])
 				attributes.append([])
-			if results_functions:
-				functions[len(classes)-1].append(results_functions.group(2))
+			if results_methods:
+				methods[len(classes)-1].append(results_methods.group(2))
 			if results_attributes:
 				attributes[len(classes)-1].append(results_attributes.group(2))
-		print(functions)
+		print(methods)
 		print(classes)
 		print(attributes)
